907fb4aaa018404cc58f8b4491ee8c0f732c1a7e
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-20 13:32:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ updateObjectWithKeyAndValue(object, key, value)
 function updateObjectWithKeyAndValue(obj,prop,value)
 {
   obj = {prop: 1}
-  var newObj = Object.assign({},obj,key.value)
+  var newObj = Object.assign({},obj,prop.value)
   return [newObj]
 }

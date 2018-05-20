2696135455ac4d725b0e3a19fd70b3affe802474
paulo.gomes@owl.ucc.edu <paulo.gomes@owl.ucc.edu>
2018-05-20 14:35:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(obj,key,value) {
 }
 function deleteFromObjectByKey(obj,key)
 {
+  
+  newObj = Object.assign({}, obj)
   delete newObj.key
+  return newObj
 }
